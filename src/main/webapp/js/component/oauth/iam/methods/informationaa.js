59041eb207b6812a741f86bf9cf674822959c6e5
MisakaMikoto <MisakaMikoto@github.com>
2016-05-02 11:22:51 +0900

    CommonRequest 구조 수정
    - CommonRequest callback 방식으로 수정
    - OauthRequest 삭제
    - layoutRequest 삭제

---
+++ src/main/webapp/js/component/oauth/iam/methods/informationaa.js
@@ -0,0 +1,34 @@
+/**
+ * Created by Misaka on 2016-05-02.
+ */
+var IAMInformation = (function() {
+    function IAMInformation() {
+    };
+
+    var _access_token = '';
+
+    IAMInformation.prototype = {
+        setAccessToken: function(access_token) {
+            _access_token = access_token;
+        },
+
+        getAccessToken: function() {
+            return _access_token;
+        },
+
+        view: function(responseText) {
+            document.getElementById('tokenInfo').value = responseText;
+        },
+
+        callRest: function(callback) {
+            var commonRequest = new CommonRequest();
+
+            commonRequest.setType('POST');
+            commonRequest.setUri('/iamOAuth/receive/tokenInfo');
+            commonRequest.setParameter('access_token=' + this.getAccessToken());
+            commonRequest.load(callback);
+        }
+    };
+
+    return IAMInformation;
+})();
