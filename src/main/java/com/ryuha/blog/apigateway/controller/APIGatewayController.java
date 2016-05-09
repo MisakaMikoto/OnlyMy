db33cd863fac48287ac81f7d6dc4d4579e2d763d
MisakaMikoto <someanspretty@gmail.com>
2016-05-09 19:23:08 +0900

    APIGateway controller 및 service 업데이트

--- src/main/java/com/ryuha/blog/apigateway/controller/APIGatewayController.java
+++ src/main/java/com/ryuha/blog/apigateway/controller/APIGatewayController.java
@@ -1,12 +1,15 @@
 package com.ryuha.blog.apigateway.controller;
 
 import com.ryuha.blog.apigateway.service.APIGatewayService;
+import org.json.JSONObject;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
 
 /**
  * Created by Misaka on 2016-05-02.
@@ -20,5 +23,32 @@ public class APIGatewayController {
     @RequestMapping(method = RequestMethod.GET)
     public String load() {
         return "apigateway/index";
+    }
+
+    @RequestMapping(value = "/call/get", method = RequestMethod.GET)
+    public @ResponseBody String callGet(HttpServletRequest request) {
+        String queryString = request.getQueryString();
+        String uri = apiGatewayService.parseUri(queryString);
+        String parameter = apiGatewayService.parseParameter(queryString);
+
+        uri = uri + "?" + parameter;
+        JSONObject response = apiGatewayService.call("GET", uri, null);
+
+        return response.toString();
+    }
+
+    @RequestMapping(value = "/call/post", method = RequestMethod.POST)
+    public @ResponseBody String callPost(@RequestBody String queryString) {
+        String uri = null;
+        try {
+            uri = apiGatewayService.parseUri(URLDecoder.decode(queryString, "UTF-8"));
+
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        String parameter = apiGatewayService.parseParameter(queryString);
+        JSONObject response = apiGatewayService.call("POST", uri, parameter);
+
+        return response.toString();
     }
 }
--- src/main/java/com/ryuha/blog/apigateway/service/APIGatewayService.java
+++ src/main/java/com/ryuha/blog/apigateway/service/APIGatewayService.java
@@ -1,11 +1,135 @@
 package com.ryuha.blog.apigateway.service;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.stereotype.Service;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 /**
  * Created by Misaka on 2016-05-02.
  */
 @Service(value = "apiGatewayService")
 public class APIGatewayService {
+    public static final String GET = "GET";
+    public static final String POST = "POST";
 
+    public JSONObject call(String type, String uri, String parameter) {
+        String urlParameters;
+        JSONObject jsonObject = null;
+
+        try {
+            URL url = new URL(uri);
+
+            if(POST.equals(type.toUpperCase()) && parameter != null) {
+                urlParameters = parameter;
+                jsonObject = this.httpURLConnectionHandler(POST, url, urlParameters);
+
+            } else {
+                jsonObject = this.httpURLConnectionHandler(GET, url, null);
+            }
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        return jsonObject;
+    }
+
+    public JSONObject httpURLConnectionHandler(String method, URL url, String urlParameters) {
+        StringBuffer responseText = null;
+        BufferedReader in = null;
+        HttpURLConnection connection = null;
+
+        try {
+            if(POST.equals(method) && urlParameters != null) {
+                connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestMethod(method);
+                connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+                connection.setDoOutput(true);
+
+                DataOutputStream wr = new DataOutputStream(connection.getOutputStream());
+                wr.writeBytes(urlParameters);
+                wr.flush();
+                wr.close();
+
+            // GET
+            } else {
+                connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestMethod(method);
+            }
+
+            System.out.println("\nSending 'POST' request to URL : " + url);
+            System.out.println("parameters : " + urlParameters);
+            System.out.println("Response Code : " + connection.getResponseCode());
+
+            in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String inputLine;
+            responseText = new StringBuffer();
+
+            while ((inputLine = in.readLine()) != null) {
+                responseText.append(inputLine);
+            }
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        } finally {
+            JSONObject jsonObject = null;
+
+            try {
+                jsonObject = new JSONObject();
+                jsonObject.put("responseCode", connection.getResponseCode());
+                jsonObject.put("responseHeaders", connection.getHeaderFields().toString());
+                jsonObject.put("responseText", (responseText != null) ? responseText.toString() : "");
+
+                if(in != null) {
+                    in.close();
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            connection.disconnect();
+
+            return jsonObject;
+        }
+    }
+
+    public String parseUri(String queryString) {
+        String uri = null;
+        if(queryString.indexOf("&") > -1) {
+            for(int i = 0; i < queryString.split("&").length; i++) {
+                if("uri".equals(queryString.split("&")[i].split("=")[0])) {
+                    uri = queryString.split("&")[i].split("=")[1];
+                }
+            }
+
+        } else {
+            uri = queryString.split("=")[1];
+        }
+        return uri;
+    }
+
+    public String parseParameter(String queryString) {
+        StringBuffer parameter = null;
+        if(queryString.indexOf("&") > -1) {
+            for (int i = 0; i < queryString.split("&").length; i++) {
+                if (!("uri".equals(queryString.split("&")[i].split("=")[0]))) {
+                    parameter.append(queryString.split("&")[i]);
+                }
+            }
+        }
+        return (parameter != null) ? parameter.toString() : "";
+    }
 }
