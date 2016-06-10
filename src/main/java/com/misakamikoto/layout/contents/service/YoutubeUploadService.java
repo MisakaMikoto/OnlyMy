974f8c0d62d38735158393bbfed1e8332f1ea6e1
MisakaMikoto <MisakaMikoto@github.com>
2016-06-10 18:58:47 +0900

    youtube uploader 추가

---
+++ src/main/java/com/misakamikoto/layout/contents/service/YoutubeUploadService.java
@@ -0,0 +1,216 @@
+package com.misakamikoto.layout.contents.service;
+
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.java6.auth.oauth2.FileCredentialStore;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.json.GoogleJsonResponseException;
+import com.google.api.client.googleapis.media.MediaHttpUploader;
+import com.google.api.client.googleapis.media.MediaHttpUploaderProgressListener;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.InputStreamContent;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.services.youtube.YouTube;
+import com.google.api.services.youtube.model.Video;
+import com.google.api.services.youtube.model.VideoSnippet;
+import com.google.api.services.youtube.model.VideoStatus;
+import com.google.common.collect.Lists;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.mvc.support.RedirectAttributesModelMap;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+/**
+ * Created by Misaka on 2016-06-01.
+ */
+@Service(value = "youtubeUploadService")
+public class YoutubeUploadService {
+    @Autowired
+    private SimpMessagingTemplate template;
+
+    /** Global instance of the HTTP transport. */
+    private static final HttpTransport HTTP_TRANSPORT = new NetHttpTransport();
+
+    /** Global instance of the JSON factory. */
+    private static final JsonFactory JSON_FACTORY = new JacksonFactory();
+
+    /** Global instance of Youtube object to make all API requests. */
+    private static YouTube youtube;
+
+    /** Global instance of the format used for the video being uploaded (MIME type). */
+    private static String VIDEO_FILE_FORMAT = "video/*";
+
+
+    /**
+     * Authorizes the installed application to access user's protected data.
+     *
+     * @param scopes list of scopes needed to run youtube upload.
+     */
+    private static Credential authorize(List<String> scopes) throws Exception {
+        // Load client secrets.
+        GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(
+                JSON_FACTORY, YoutubeUploadService.class.getResourceAsStream("/youtube/client_secrets.json"));
+
+        // Set up file credential store.
+        FileCredentialStore credentialStore = new FileCredentialStore(
+                new File(System.getProperty("user.home"), ".credentials/youtube-api-uploadvideo.json"),
+                JSON_FACTORY);
+
+        // Set up authorization code flow.
+        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
+                HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, scopes).setCredentialStore(credentialStore)
+                .build();
+
+        // Build the local server and bind it to port 9000
+        LocalServerReceiver localReceiver = new LocalServerReceiver.Builder().setPort(9000).build();
+
+        // Authorize.
+        return new AuthorizationCodeInstalledApp(flow, localReceiver).authorize("user");
+    }
+
+    public String upload(MultipartFile file, String title, String description, String tags) {
+        Video returnedVideo = null;
+
+        // Scope required to upload to YouTube.
+        List<String> scopes = Lists.newArrayList("https://www.googleapis.com/auth/youtube.upload");
+
+        try {
+            // Authorization.
+            Credential credential = authorize(scopes);
+
+            // YouTube object used to make all API requests.
+            youtube = new YouTube.Builder(HTTP_TRANSPORT, JSON_FACTORY, credential).setApplicationName(
+                    "youtube-cmdline-uploadvideo-sample").build();
+
+            // We get the user selected local video file to upload.
+            File videoFile = new File(file.getName());
+            file.transferTo(videoFile);
+            System.out.println("You chose " + videoFile + " to upload.");
+
+            // Add extra information to the video before uploading.
+            Video videoObjectDefiningMetadata = new Video();
+
+      /*
+       * Set the video to public, so it is available to everyone (what most people want). This is
+       * actually the default, but I wanted you to see what it looked like in case you need to set
+       * it to "unlisted" or "private" via API.
+       */
+            VideoStatus status = new VideoStatus();
+            status.setPrivacyStatus("public");
+            videoObjectDefiningMetadata.setStatus(status);
+
+            // We set a majority of the metadata with the VideoSnippet object.
+            VideoSnippet snippet = new VideoSnippet();
+
+      /*
+       * The Calendar instance is used to create a unique name and description for test purposes, so
+       * you can see multiple files being uploaded. You will want to remove this from your project
+       * and use your own standard names.
+       */
+            Calendar cal = Calendar.getInstance();
+            snippet.setTitle(title);
+            snippet.setDescription(description + "on " + cal.getTime());
+
+            // Set your keywords.
+            List<String> tagList = new ArrayList<String>();
+            String[] parseTags = tags.split(",");
+            for(String tag : parseTags) {
+                tagList.add(tag);
+            }
+            snippet.setTags(tagList);
+
+            // Set completed snippet to the video object.
+            videoObjectDefiningMetadata.setSnippet(snippet);
+
+            InputStreamContent mediaContent = new InputStreamContent(
+                    VIDEO_FILE_FORMAT, new BufferedInputStream(new FileInputStream(videoFile)));
+            mediaContent.setLength(videoFile.length());
+
+      /*
+       * The upload command includes: 1. Information we want returned after file is successfully
+       * uploaded. 2. Metadata we want associated with the uploaded video. 3. Video file itself.
+       */
+            YouTube.Videos.Insert videoInsert = youtube.videos()
+                    .insert("snippet,statistics,status", videoObjectDefiningMetadata, mediaContent);
+
+            // Set the upload type and add event listener.
+            MediaHttpUploader uploader = videoInsert.getMediaHttpUploader();
+
+      /*
+       * Sets whether direct media upload is enabled or disabled. True = whole media content is
+       * uploaded in a single request. False (default) = resumable media upload protocol to upload
+       * in data chunks.
+       */
+            uploader.setDirectUploadEnabled(false);
+
+            MediaHttpUploaderProgressListener progressListener = new MediaHttpUploaderProgressListener() {
+                public void progressChanged(MediaHttpUploader uploader) throws IOException {
+                    switch (uploader.getUploadState()) {
+                        case INITIATION_STARTED:
+                            System.out.println("Initiation Started");
+                            break;
+
+                        case INITIATION_COMPLETE:
+                            System.out.println("Initiation Completed");
+                            template.convertAndSend("/push", "iCompleted");
+                            break;
+
+                        case MEDIA_IN_PROGRESS:
+                            System.out.println("Upload in progress");
+                            System.out.println("Upload percentage: " + uploader.getProgress());
+                            template.convertAndSend("/push", uploader.getProgress());
+                            break;
+
+                        case MEDIA_COMPLETE:
+                            System.out.println("Upload Completed!");
+                            template.convertAndSend("/push", "uCompleted");
+                            break;
+
+                        case NOT_STARTED:
+                            System.out.println("Upload Not Started!");
+                            break;
+                    }
+                }
+            };
+            uploader.setProgressListener(progressListener);
+
+            // Execute upload.
+            returnedVideo = videoInsert.execute();
+
+            // Print out returned results.
+            System.out.println("\n================== Returned Video ==================\n");
+            System.out.println("  - Id: " + returnedVideo.getId());
+            System.out.println("  - Title: " + returnedVideo.getSnippet().getTitle());
+            System.out.println("  - Tags: " + returnedVideo.getSnippet().getTags());
+            System.out.println("  - Privacy Status: " + returnedVideo.getStatus().getPrivacyStatus());
+            System.out.println("  - Video Count: " + returnedVideo.getStatistics().getViewCount());
+
+        } catch (GoogleJsonResponseException e) {
+            System.err.println("GoogleJsonResponseException code: " + e.getMessage());
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.err.println("IOException: " + e.getMessage());
+            e.printStackTrace();
+        } catch (Throwable t) {
+            System.err.println("Throwable: " + t.getMessage());
+            t.printStackTrace();
+        }
+
+        return returnedVideo.getId();
+    }
+}
