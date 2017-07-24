8f65b9872cc3a1d377223389d7b23e9dbe2bc73d
MisakaMikoto <someanspretty@gmail.com>
2017-07-24 15:53:40 +0900

    modify main controller ( String -> ModelAndView)

--- src/main/java/com/misakamikoto/main/controller/MainController.java
+++ src/main/java/com/misakamikoto/main/controller/MainController.java
@@ -3,12 +3,21 @@ package com.misakamikoto.main.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
+/**
+ * The type Main controller.
+ */
 @RestController
 @RequestMapping("/")
 public class MainController {
+	/**
+	 * Load string.
+	 *
+	 * @return the string
+	 */
 	@RequestMapping(method = RequestMethod.GET)
-	public String load() {
-		return "main/index";
+	public ModelAndView load() {
+		return new ModelAndView("main/index");
 	}
 }
